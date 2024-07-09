e29cb8d1b5a283005b46ef0b2b2ba76db6068830
theendercore <theendercore@gmail.com>
2024-07-09 19:23:26 +0300

    - MiscInit - rotten_flesh can be composted

--- src/main/kotlin/org/teamvoided/uuim/UsefulUselessItemsMod.kt
+++ src/main/kotlin/org/teamvoided/uuim/UsefulUselessItemsMod.kt
@@ -3,6 +3,7 @@ package org.teamvoided.uuim
 import net.minecraft.util.Identifier
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.teamvoided.uuim.init.MiscInit
 
 @Suppress("unused")
 object UsefulUselessItemsMod {
@@ -12,7 +13,9 @@ object UsefulUselessItemsMod {
     val log: Logger = LoggerFactory.getLogger(UsefulUselessItemsMod::class.simpleName)
 
     fun init() {
-        log.info("Hello from Common")
+        log.info("You *WILL* be made useful!")
+
+        MiscInit.init()
     }
 
     fun id(path: String) = Identifier.of(MODID, path)
---
+++ src/main/kotlin/org/teamvoided/uuim/init/MiscInit.kt
@@ -0,0 +1,12 @@
+package org.teamvoided.uuim.init
+
+import net.minecraft.block.ComposterBlock
+import net.minecraft.item.Items
+
+object MiscInit {
+
+    fun init() {
+
+        ComposterBlock.ITEM_TO_LEVEL_INCREASE_CHANCE.put(Items.ROTTEN_FLESH, 0.1f)
+    }
+}
